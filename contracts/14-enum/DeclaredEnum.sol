8add621e9e6d1bc6ef8927becce8ceffd5af792e
versgrid <versegrid@gmail.com>
2024-06-17 08:23:01 -0400

    struct

---
+++ contracts/14-enum/DeclaredEnum.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+// This is saved 'DeclaredEnum.sol'
+
+enum Status {
+    Pending,
+    Shipped,
+    Accepted,
+    Rejected,
+    Canceled
+}
